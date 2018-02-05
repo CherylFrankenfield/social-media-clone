03ec82167a1ef8660109e0854f0453dc73a31c55
Adam Smith and Cheryl Frankenfield <cheryl@>
2018-02-05 14:54:05 -0800

    Add Eslint configurations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Header from './Header';
+import Body from './Body';
 
 function App() {
   return(
+    <Header />
+    <Body />
     <span>App works!</span>
   );
 }
